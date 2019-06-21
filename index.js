c5fa614a1ae2eacb51b6ec94eb10b87338c14466
JacobABrennan <jacobabrennan@gmail.com>
2019-06-21 18:14:00 -0400

    Add comments/documentation to index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 
-//== Start Server ==============================================================
+/*== Start Server ==============================================================
+
+This is the start script for the wargames webserver. It is separated from the
+webserver definition to allow automated testing.
+
+*/
 
 //-- Dependencies --------------------------------
 const webserver = require('./webserver.js');
